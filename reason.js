b01bca4aefda3d1b6d7cd2a15b1dac8d3185c5ac
Ryan Gough <ryan.gough@planixs.com>
2016-11-12 00:06:22 +0000

    add basic unification

--- reason.js
+++ reason.js
@@ -1,28 +1,65 @@
-module.exports = {
-    newSub: function(){
-        return {
-            assoc: {},
-            isEmpty: function(){
-                return Object.getOwnPropertySymbols(this.assoc).length === 0;
-            },
-            extend: function(variable, value){
-                this.assoc[variable] = value;
-                return this;
-            },
-            lookup: function(variable){
-                return this.assoc[variable];
-            },
-            reify: function(){
-                var me = this;
-                var vars = Object.getOwnPropertySymbols(this.assoc);
-                return vars.reduce(function(a,i){
-                    a.push(i.toString() + ":" + me.lookup(i));
-                    return a;
-                },[]).join(", "); 
+function newSub(){
+    return {
+        assoc: {},
+        isEmpty: function(){
+            return Object.getOwnPropertySymbols(this.assoc).length === 0;
+        },
+        lookup: function(variable){
+            if (!this.assoc.hasOwnProperty(variable)){
+                return variable;
             }
+            return this.assoc[variable];
+        },
+        reify: function(){
+            var me = this;
+            var vars = Object.getOwnPropertySymbols(this.assoc);
+            return vars.reduce(function(a,i){
+                a.push(i.toString() + ":" + me.lookup(i));
+                return a;
+            },[]).join(", "); 
+        },
+        extend: function(variable, value){
+            var subE = newSub();
+            Object.assign(subE.assoc, this.assoc);
+            subE.assoc[variable] = value;
+            return subE;
         }
-    },
-    fresh: function(desc){
-        return Symbol(desc);
     }
+};
+
+function fresh(desc){
+    return Symbol(desc);
+};
+
+function isFresh(variable){
+    return typeof variable === 'symbol';
+}
+
+function unify(x, y, s){
+    var xLookup = s.lookup(x);
+    var yLookup = s.lookup(y);
+    if (isFresh(xLookup)){
+        return success(s.extend(xLookup, yLookup));
+    }
+    if (isFresh(yLookup)){
+        return success(s.extend(yLookup, xLookup));
+    }
+    if (xLookup === yLookup){
+        return success(s);
+    }
+    return fail();
+};
+
+function success(s){
+    return [s];
+};
+
+function fail(){
+    return [];
+};
+
+module.exports = {
+    newSub: newSub,
+    fresh: fresh,
+    unify: unify
 };
--- test-reason.js
+++ test-reason.js
@@ -7,7 +7,6 @@ x = r.fresh("x");
 y = r.fresh("y");
 
 run([
-
     function a_new_substitution_should_be_empty(){
         var s = r.newSub();
         assert(s.isEmpty(), "a new substitution should be empty");
@@ -15,22 +14,52 @@ run([
 
     function a_substitution_can_be_extended_with_a_new_association(){
         var s = r.newSub();
-        s.extend(x, "foo");
-        assert(!s.isEmpty(), "failed to extend substitution");
+        var s1 = s.extend(x, "foo");
+        assert(!s1.isEmpty(), "failed to extend substitution");
     },
 
     function a_variables_association_can_be_retrieved_from_a_substitution(){
         var s = r.newSub();
-        s.extend(x, "foo");
-        var result = s.lookup(x);
+        var s1 = s.extend(x, "foo");
+        var result = s1.lookup(x);
         assert.equal(result, "foo", "failed to lookup association");
     },
 
     function a_substitution_can_be_reified(){
         var s = r.newSub();
-        s.extend(x, "foo");
-        s.extend(y, "bar");
-        var reification = s.reify();
+        var s1 = s.extend(x, "foo");
+        var s2 = s1.extend(y, "bar");
+        var reification = s2.reify();
         assert.equal(reification, "Symbol(x):foo, Symbol(y):bar", "failed to reify substitution : " + reification);
     },
+
+    function a_fresh_variable_can_be_unified_with_a_value(){
+        var substream = r.unify(x, "foo", r.newSub());
+        assert.equal(substream[0].lookup(x), "foo", "failed to unify fresh variable with value");
+    },
+
+    function a_value_can_be_unified_with_a_fresh_variable(){
+        var substream = r.unify("foo", x, r.newSub());
+        assert.equal(substream[0].lookup(x), "foo", "failed to unify value with fresh variable");
+    },
+
+    function a_non_fresh_variable_cannot_have_its_value_changed(){
+        var s = r.newSub();
+        var s1 = s.extend(x, "foo");
+        var substream = r.unify(x, "bar", s1);
+        assert.equal(substream.length, 0, "non fresh variable cannot have its value changed");
+    },
+
+    function two_identical_values_unify_without_extending_substitution(){
+        var s = r.newSub();
+        var substream = r.unify("foo", "foo", s);
+        assert.equal(substream[0], s, "failed to unify identical values");
+    },
+
+    function two_different_values_fail_to_unify(){
+        var s = r.newSub();
+        var substream = r.unify("foo", "bar", s);
+        assert.equal(substream.length, 0, "two different values should fail to unify");
+    }
+
 ])
